92c7b58263759ef1ccdbd00a6a1a2cf06a9e1016
mfg25 <unnmfg@usp.br>
2023-03-22 21:30:37 -0300

    responsive

--- src/components/mainPage/video-container.js
+++ src/components/mainPage/video-container.js
@@ -1,26 +1,25 @@
-import './video-container.css';
-import Video from './background.gif';
+import "./video-container.css";
+import Video from "./background.gif";
 
-function backgroundVideo(){
+function backgroundVideo() {
+  let divContainer = document.createElement("div");
+  divContainer.id = "video-container";
 
-    let divContainer = document.createElement('div')
-    divContainer.id = 'video-container'
-    
-    let backgroundVideo = document.createElement('img')
-    backgroundVideo.src = Video
-    backgroundVideo.id = 'gif'
+  let backgroundVideo = document.createElement("img");
+  backgroundVideo.src = Video;
+  backgroundVideo.id = "gif";
 
-    let introductionText = document.createElement('h1')
-    introductionText.id = 'introduction'
-    introductionText.innerText = `Take free online classes and courses in astronomy to build your
+  let introductionText = document.createElement("h1");
+  introductionText.id = "introduction";
+  introductionText.innerText = `Take free online classes and courses in astronomy to build your
     science skills. Explore astronomy, astrophysics and other popular
     subjects with courses from top universities and institutions around
-    the world.`
+    the world.`;
 
-    divContainer.appendChild(backgroundVideo)
-    divContainer.appendChild(introductionText)
-    
-    return divContainer
+  divContainer.appendChild(backgroundVideo);
+  divContainer.appendChild(introductionText);
+
+  return divContainer;
 }
 
-export default backgroundVideo
+export default backgroundVideo;
